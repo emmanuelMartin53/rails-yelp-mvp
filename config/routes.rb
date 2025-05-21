d63ae680422e60e347dce6127ca8b9b88ab2a56e
Emmanuel Martin <em.martin456@gmail.com>
2025-05-21 16:57:06 +0200

    delete method done

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "restaurants", to: "restaurants#index", as: :restaurants
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 
-  get "restaurants", to: "restaurants#index", as: :restaurants
 
-  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
-  post "restaurants", to: "restaurants#create"
-
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-
-  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
-  patch "restaurants/:id", to: "restaurants#update"
 end

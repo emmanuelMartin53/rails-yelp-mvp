c9eb9967192ac585b1af49e67623a2ebd248e153
Emmanuel Martin <em.martin456@gmail.com>
2025-05-21 14:19:49 +0200

    Route get restaurants: done

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   validates :content, :rating , presence: true
   validates :rating, numericality: {
     only_integer: true,
-    in: 0..5, # Cette syntaxe est disponible à partir de Rails 6.1
+    in: 0..5, 
     message: "doit être un entier entre 0 et 5"
     }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Big Fernand", address: "14 rue du lac, Bordeaux", phone_number: "0556896523" ,category: "french")
+puts "Created Big Fernand"
+Restaurant.create!(name: "Peppone", address: "14 rue servandoni, Bordeaux", phone_number: "0554321523" ,category: "italian")
+puts "Created Peppone"
+Restaurant.create!(name: "Mitsuba", address: "67 place Gambetta, Bordeaux", phone_number: "0565230102" ,category: "japanese")
+puts "Created Mitsuba"
+Restaurant.create!(name: "Panda", address: "14 rue de Chine, Bordeaux", phone_number: "0123456789" ,category: "chinese")
+puts "Created Panda"
+Restaurant.create!(name: "Moules-frites", address: "28 rue des remparts, Bordeaux", phone_number: "098765432" ,category: "belgian")
+puts "Created Moules-frites"
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."

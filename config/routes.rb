84198b0da2d98522d6eecc576347de0b7211240c
Emmanuel Martin <em.martin456@gmail.com>
2025-05-21 16:21:55 +0200

    Update method Done

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "restaurants", to: "restaurants#index", as: :restaurants
+
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
 end
